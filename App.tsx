5e7a404267c43193e53f6964f4c1d892f3820b3d
Christian Castellanos <47896192+c4chris124@users.noreply.github.com>
2023-03-24 12:27:14 -0600

    setting up react native navigation library, adding stack navigator component, imported gesture handler on index

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+    </NavigationContainer>
   )
 }
 
