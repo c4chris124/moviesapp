bbb5247441f8712cfff4d937ca96493ae2d4215c
Christian Castellanos <47896192+c4chris124@users.noreply.github.com>
2023-03-24 18:58:57 -0600

    Created movie poster componet, added props and interface, and styles

--- src/components/MoviePoster.tsx
+++ src/components/MoviePoster.tsx
@@ -1,12 +1,48 @@
 import React from 'react'
-import { Text, View } from 'react-native';
+import { Text, View, Image, StyleSheet } from 'react-native';
+import { Movie } from '../interfaces/movieInterface';
 
-export const MoviePoster = () => {
+interface Props {
+    movie: Movie
+}
+
+export const MoviePoster = ({ movie }: Props) => {
+
+    const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+
     return (
-        <View>
-            <Text>
-                Movie poster
-            </Text>
+        <View style={{
+            width: 300,
+            height: 420,
+        }}>
+            <View style={styles.imageContainer}>
+                <Image
+                    source={{
+                        uri
+                    }}
+                    style={styles.image}
+                />
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    image: {
+        flex: 1,
+        borderRadius: 18,
+    },
+    imageContainer: {
+        flex: 1,
+        borderRadius: 18,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 7,
+        },
+        shadowOpacity: 0.41,
+        shadowRadius: 9.11,
+
+        elevation: 14,
+    }
+});
