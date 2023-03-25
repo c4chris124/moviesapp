3536032c0a50ea6487c0cd63240cfd10643743f0
Christian Castellanos <47896192+c4chris124@users.noreply.github.com>
2023-03-24 18:04:05 -0600

    imported custome hook, use movies and created loading conditional

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,17 +1,27 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ActivityIndicator } from 'react-native'
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParams } from '../navigation/Navigation'
+import { useMovies } from '../hooks/useMovies';
+import { MoviePoster } from '../components/MoviePoster';
 
 interface Props extends StackScreenProps<RootStackParams, 'HomeScreen'> { }
 
 export const HomeScreen = ({ navigation }: Props) => {
+    const { moviesNowPlaying, isLoading } = useMovies()
+
+
+    if (isLoading) {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignContent: 'center' }}>
+                <ActivityIndicator color="red" size={50} />
+            </View>
+        )
+    }
 
     return (
         <View>
-            <Text>
-                HomeScreen
-            </Text>
+            <MoviePoster />
         </View>
     )
 }
