16204121330a0351285eb8eebf80c47b47c9c8c8
Christian Castellanos <47896192+c4chris124@users.noreply.github.com>
2023-03-28 17:03:24 -0600

    Adding styling

--- src/components/MoviePoster.tsx
+++ src/components/MoviePoster.tsx
@@ -4,16 +4,19 @@ import { Movie } from '../interfaces/movieInterface';
 
 interface Props {
     movie: Movie
+    height?: number
+    width?: number
 }
 
-export const MoviePoster = ({ movie }: Props) => {
+export const MoviePoster = ({ movie, height = 420, width = 300 }: Props) => {
 
     const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
 
     return (
         <View style={{
-            width: 300,
-            height: 420,
+            width,
+            height,
+            marginHorizontal: 8
         }}>
             <View style={styles.imageContainer}>
                 <Image
@@ -38,11 +41,11 @@ const styles = StyleSheet.create({
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
-            height: 7,
+            height: 10,
         },
-        shadowOpacity: 0.41,
-        shadowRadius: 9.11,
+        shadowOpacity: 0.24,
+        shadowRadius: 7,
 
-        elevation: 14,
+        elevation: 9,
     }
 });
