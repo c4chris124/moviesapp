0eaa41a9187958cc4ad4918413e67e7527926dec
Christian Castellanos <47896192+c4chris124@users.noreply.github.com>
2023-03-28 17:50:51 -0600

    refactor hook, one state for all request, created interface movi state, use state movies, promise all for movies

--- src/hooks/useMovies.tsx
+++ src/hooks/useMovies.tsx
@@ -1,14 +1,43 @@
 import { useEffect, useState } from 'react'
 import movieDB from '../api/MovieDB';
-import { MovieDbNowPlaying, Movie } from '../interfaces/movieInterface';
+import { MovieDBResponse, Movie } from '../interfaces/movieInterface';
+
+interface MoviesState {
+    nowPlaying: Movie[]
+    popular: Movie[]
+    topRated: Movie[]
+    upcoming: Movie[]
+}
 
 export const useMovies = () => {
     const [isLoading, setIsLoading] = useState(true)
-    const [moviesNowPlaying, setMoviesNowPlaying] = useState<Movie[]>([])
+    const [moviesState, setMoviesSate] = useState<MoviesState>({
+        nowPlaying: [],
+        popular: [],
+        topRated: [],
+        upcoming: [],
+    })
 
     const getMovies = async () => {
-        const res = await movieDB.get<MovieDbNowPlaying>('/now_playing')
-        setMoviesNowPlaying(res.data.results)
+        const nowPlayingPromise = movieDB.get<MovieDBResponse>('/popular')
+        const popularPromise = movieDB.get<MovieDBResponse>('/top_rated')
+        const topRatedPromise = movieDB.get<MovieDBResponse>('/now_playing')
+        const upcomingPromise = movieDB.get<MovieDBResponse>('/upcoming')
+
+        const res = await Promise.all([
+            nowPlayingPromise,
+            popularPromise,
+            topRatedPromise,
+            upcomingPromise,
+        ])
+
+        setMoviesSate({
+            nowPlaying: res[0].data.results,
+            popular: res[1].data.results,
+            topRated: res[2].data.results,
+            upcoming: res[3].data.results,
+        })
+
         setIsLoading(false)
     }
 
@@ -18,7 +47,7 @@ export const useMovies = () => {
     }, [])
 
     return {
-        moviesNowPlaying,
-        isLoading
+        ...moviesState,
+        isLoading,
     }
 }
