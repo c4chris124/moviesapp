fc1e5397e74d7bbd1c56017bccca1789fb93cb81
Christian Castellanos <47896192+c4chris124@users.noreply.github.com>
2023-03-28 19:45:54 -0600

    Added interface to get movie data

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { StackScreenProps } from '@react-navigation/stack';
+import { RootStackParams } from '../navigation/Navigation';
 
-export const DetailScreen = () => {
+
+interface Props extends StackScreenProps<RootStackParams, 'DetailScreen'> {
+
+}
+
+export const DetailScreen = ({ route }: Props) => {
+
+    const movie = route.params
+
     return (
         <View>
             <Text>
