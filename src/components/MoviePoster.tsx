4deb0968936b945b38cdf07f28f1e3505717b280
Christian Castellanos <47896192+c4chris124@users.noreply.github.com>
2023-03-28 19:42:02 -0600

    Added usenavigation hook to navigate to details, pass movie data

--- src/components/MoviePoster.tsx
+++ src/components/MoviePoster.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import { Text, View, Image, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
+import { View, Image, StyleSheet } from 'react-native';
 import { Movie } from '../interfaces/movieInterface';
+import { TouchableOpacity } from 'react-native';
 
 interface Props {
     movie: Movie
@@ -12,12 +14,19 @@ export const MoviePoster = ({ movie, height = 420, width = 300 }: Props) => {
 
     const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
 
+    const navigator = useNavigation()
+
+
     return (
-        <View style={{
-            width,
-            height,
-            marginHorizontal: 8
-        }}>
+        <TouchableOpacity
+            onPress={() => navigator.navigate('DetailScreen', movie)}
+            activeOpacity={0.9}
+            style={{
+                width,
+                height,
+                marginHorizontal: 8
+            }}
+        >
             <View style={styles.imageContainer}>
                 <Image
                     source={{
@@ -26,7 +35,7 @@ export const MoviePoster = ({ movie, height = 420, width = 300 }: Props) => {
                     style={styles.image}
                 />
             </View>
-        </View>
+        </TouchableOpacity>
     )
 }
 
